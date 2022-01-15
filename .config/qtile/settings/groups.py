8adf16470d46268f217daeeb10503a3ef41bfc84
Nicolás C. Ibarra <ricardoibarra2044@gmail.com>
2022-01-15 17:27:13 -0600

    fix qtile to switch last workspace

--- .config/qtile/settings/groups.py
+++ .config/qtile/settings/groups.py
@@ -19,7 +19,7 @@ for i, group in enumerate(groups):
     actual_key = str(i + 1)
     keys.extend([
         # Switch to workspace N
-        Key([mod], actual_key, lazy.group[group.name].toscreen()),
+        Key([mod], actual_key, lazy.group[group.name].toscreen(toggle=True)),
         # Send window to workspace N
         Key([mod, "shift"], actual_key, lazy.window.togroup(group.name))
     ])
